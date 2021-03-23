f7f7571d38fe886dda95de1b7983fbcb391e9c62
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-23 14:53:06 +0100

    initial add from Spring Initilizr

---
+++ src/main/java/matt/project/spring5jokeappv2/Spring5JokeAppV2Application.java
@@ -0,0 +1,13 @@
+package matt.project.spring5jokeappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokeAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokeAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/matt/project/spring5jokeappv2/Spring5JokeAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package matt.project.spring5jokeappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
